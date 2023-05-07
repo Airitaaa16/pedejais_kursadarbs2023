2cb67a747d7f2e240c489d86304a435cb99f218e
Airitaaa16 <102229277+Airitaaa16@users.noreply.github.com>
2023-05-07 19:29:39 +0300

    mhh

---
+++ akvarija_laukums.js
@@ -0,0 +1,13 @@
+function poga (){
+    var g, p, izvade;
+    g = Number(garums.value);
+    p = Number(platums.value);
+    if(isNaN(g) || isNaN(p)) {
+        izvade = "Kļūda datu ievadē.";
+    }
+    else {
+        izvade = "Akvārija laukums ir" + g * p + ".";
+
+    }
+    rezultāts.innerHTML = izvade;
+}
