0afdc2c2eb2f6f507358b87cce7b3934f65b31d4
Airitaaa16 <102229277+Airitaaa16@users.noreply.github.com>
2023-05-10 13:31:29 +0300

    labi pastrādāju

--- akvarija_laukums.js
+++ akvarija_laukums.js
@@ -1,5 +1,20 @@
 
-function poga (){
+function laukums (){
+    var g, p, izvade;
+    g = Number(garums.value);
+    p = Number(platums.value);
+    if(isNaN(g) || isNaN(p)) {
+        izvade = "Kļūda datu ievadē.";
+    }
+    else {
+        izvade = "Akvārija laukums ir" + g * p + ".";
+
+    }
+    rezultāts.innerHTML = izvade;
+}
+
+
+function tilpums (){
     var g, p, izvade;
     g = Number(garums.value);
     p = Number(platums.value);
